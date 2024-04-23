c9158d784a04d90938271670e57dd2a75ab29804
lucaslegoff3 <lucas.le-goff@mmibordeaux.com>
2024-04-23 17:37:00 +0200

    Update Functionality Automatic Up And Down
    (:

---
+++ Assets/Scripts/ObstacleBehaviours/HorizontalAutomaticMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalAutomaticMove : MonoBehaviour
+{
+	[SerializeField] private float moveSpeed = 5f; //Vitesse de l'objet, modifiable
+	[SerializeField] private int xSens = 1; //Le sens de l'objet (1 si en bas, -1 si en haut)
+	private Vector2 movement;
+
+	//Au démarrage, défini la variable de mouvement
+	void Start(){
+		movement = new Vector2(xSens, 0);
+	}
+
+	//A chaque frame, on bouge l'objet via son rigidbody dans le mouvement défini * la vitesse de l'objet moveSpeed * Time.fixedDeltaTime le laps de temps écoulé en 1 frame
+	void FixedUpdate() {
+		transform.Translate(movement * moveSpeed * Time.fixedDeltaTime);
+	}
+}
