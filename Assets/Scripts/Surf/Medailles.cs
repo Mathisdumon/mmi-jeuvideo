949f66313c3c8e60247e4faacaec3104ab31da48
lucaslegoff3 <lucas.le-goff@mmibordeaux.com>
2024-04-25 14:26:09 +0200

    Update Medailles.cs

--- Assets/Scripts/Surf/Medailles.cs
+++ Assets/Scripts/Surf/Medailles.cs
@@ -8,8 +8,10 @@ public class Medailles : MonoBehaviour
     [SerializeField] private GameObject medaille_or;
     [SerializeField] private GameObject medaille_argent;
     [SerializeField] private GameObject medaille_bronze;
+    [SerializeField] private GameObject textWinner;
 
     public void showMedaille(int medaille){
+        textWinner.SetActive(true);
         if(medaille == 1){
             medaille_or.SetActive(true);
         } else if(medaille == 2){
